67e690f6bb3d428241798ba1e721211ef68792ed
Grant Cottingham <54192928+gcotty@users.noreply.github.com>
2026-09-02 09:30:29 -0400

    Update init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -403,7 +403,7 @@ do
   -- default behavior. For example, here we set the section for
   -- cursor location to LINE:COLUMN
   ---@diagnostic disable-next-line: duplicate-set-field
-  statusline.section_location = function() return '%2l:%-2v' end
+  statusline.section_location = function() return '%2l:%-2v %L ln %p%%' end
 
   -- ... and there is more!
   --  Check out: https://github.com/nvim-mini/mini.nvim
